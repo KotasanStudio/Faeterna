1b3ddafb591994acc748001a93cab44c09ddb655
BaenaArnau <abaena300@gmail.com>
2026-04-13 16:14:48 +0200

    Add arbolpocho tileset & update Nivel1 map
    Add a new tileset (assets/FaeternaTiled/arbolpocho.tsx) and integrate it into the Nivel1 map. Update scenes/Maps/Nivel1.tmx to reference the new tileset, adjust map dimensions and layer chunk coordinates/sizes to match the updated layout, and update Tiled session metadata (active file, open/recent files, tileset tileSize and scaleInDock values). Also include corresponding scene adjustments for Nivel1 to reflect the map/tileset changes.

---
+++ assets/FaeternaTiled/arbolpocho.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="arbolpocho" tilewidth="600" tileheight="640" tilecount="2" columns="2">
+ <image source="ArbustoFaeterna4.png" width="1200" height="640"/>
+</tileset>
