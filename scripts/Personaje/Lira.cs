503769f036eb71374f6e670756794359dfba6942
MrWezh <zhouwenkai16@gmail.com>
2026-05-11 17:06:27 +0200

    HotFix.4: error dobleJump fixed

--- scripts/Personaje/Lira.cs
+++ scripts/Personaje/Lira.cs
@@ -1,7 +1,7 @@
 
 using Godot;
 using Godot.Collections;
-using Faeterna.scripts.Tools;
+using Faeterna.Scripts.Tools;
 using System.Threading.Tasks;
 using Faeterna.Scripts.Personaje.MaquinasDeEstados.Movimiento;
 
