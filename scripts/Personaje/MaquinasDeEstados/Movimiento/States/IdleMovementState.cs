597410332e5c2c92cfd3a094e8b89f8c20792b09
MrWezh <zhouwenkai16@gmail.com>
2026-05-14 20:03:32 +0200

    HotFix 4

--- scripts/Personaje/MaquinasDeEstados/Movimiento/States/IdleMovementState.cs
+++ scripts/Personaje/MaquinasDeEstados/Movimiento/States/IdleMovementState.cs
@@ -30,9 +30,7 @@ namespace Faeterna.Scripts.Personaje.MaquinasDeEstados.Movimiento.States
             {
                 GD.Print("Transitioning to falling or jumping state from idle.");
                 // En 2D, Y positivo = hacia abajo, negativo = hacia arriba (subiendo).
-                stateMachine.TransitionTo(_player.Velocity.Y < 0
-                    ? "JumpingMovementState"
-                    : "FallingMovementState");
+                stateMachine.TransitionTo("FallingMovementState");
             }
             else
             {
