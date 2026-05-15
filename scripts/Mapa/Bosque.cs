05f87e6c7a6007634d3c4f4e896e514c339be9bd
MrWezh <zhouwenkai16@gmail.com>
2026-05-15 19:46:09 +0200

    background audioStreamPlayer2D change to audioStreamPlayer

--- scripts/Mapa/Bosque.cs
+++ scripts/Mapa/Bosque.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Bosque : Node2D
 {
 
-    [Export] private AudioStreamPlayer2D _backgroundMusic;
+    [Export] private AudioStreamPlayer _backgroundMusic;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
