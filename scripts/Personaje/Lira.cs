c7aa6a3c32cdb7dc7b25ab39e167d7a859f762ad
Macnacka <abaena300@gmail.com>
2026-05-16 23:17:04 +0200

    Hotfix

--- scripts/Personaje/Lira.cs
+++ scripts/Personaje/Lira.cs
@@ -101,10 +101,10 @@ namespace Faeterna.Scripts.Personaje
         private bool _tutorial = false;
 
         /// <summary>Flags de habilidades. Estas variables indican si el personaje ha adquirido ciertas habilidades (doble salto, dash) a lo largo del juego. Son usadas por la máquina de estados para determinar qué acciones están disponibles para el jugador en cada momento. Aunque podrían ser parte de un sistema de progresión más complejo, en este caso se manejan directamente en el personaje para simplificar su acceso desde los estados de movimiento.</summary>
-        public bool _haveDobleJump { get; set;} = true;
+        public bool _haveDobleJump { get; set;} = false;
 
         /// <summary>Flags de habilidades. Estas variables indican si el personaje ha adquirido ciertas habilidades (doble salto, dash) a lo largo del juego. Son usadas por la máquina de estados para determinar qué acciones están disponibles para el jugador en cada momento. Aunque podrían ser parte de un sistema de progresión más complejo, en este caso se manejan directamente en el personaje para simplificar su acceso desde los estados de movimiento.</summary>
-        public bool _haveDash { get; set;} = true;
+        public bool _haveDash { get; set;} = false;
 
         /// <summary>Referencias a nodos de la interfaz y otros elementos relacionados con la salud, maná, muerte y descripción de objetos. Estos nodos se asignan desde el editor y se usan para actualizar visualmente la interfaz (corazones, barra de maná) y mostrar pantallas de muerte o descripciones de objetos cuando sea necesario. Aunque podrían estar gestionados por un sistema de UI separado, en este caso se incluyen directamente en el personaje para facilitar su acceso desde los estados de movimiento y otros métodos del personaje.</summary>
         [Export] public DeathScreen  _deathScreen;
